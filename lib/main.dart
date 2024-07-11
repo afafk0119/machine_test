951026fd64df413c7dcb15d68a839d3587c187ea
AfafKuppanath <afaf.k@lanwaresolutions.com>
2024-07-11 15:47:14 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter De',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  const Webviewpage(),
+      home: const Webviewpage(),
     );
   }
 }
